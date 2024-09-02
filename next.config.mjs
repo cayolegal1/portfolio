fcf7bb56a610119ec07aca51d83700976e1799f4
Cayo Legal <cayolegal@hotmail.com>
2024-09-02 02:37:24 -0400

    update next config headers and optimizing experimental imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self'",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
+  experimental: {
+    optimizePackageImports: [
+        "react-type-animation"
+    ],
+  }
+};
 
 export default nextConfig;
--- src/components/Hero/components/HeroContact/index.tsx
+++ src/components/Hero/components/HeroContact/index.tsx
@@ -5,20 +5,31 @@ import LinkedinIcon from "@/core/components/Icons/LinkedinIcon";
 import styles from "./HeroContact.module.css";
 import data from "@/core/data/info.json";
 
+const { github_url, linkedin_url } = data;
+
+const contactSources = [
+  { ariaLabel: "Visit my repository on github", href: github_url, icon: <GitHubIcon /> },
+  { ariaLabel: "Visit my profile on LinkedIn", href: linkedin_url, icon: <LinkedinIcon /> },
+];
+
 export default function HeroContact(): JSX.Element {
-  const { github_url, linkedin_url } = data;
   return (
     <AnimatedRender
       animationType="fadeInDown"
       delay="2.5s"
       className={styles.social_media_container}
     >
-      <Link href={github_url} target="_blank" className={styles.social_media_icon}>
-        <GitHubIcon />
-      </Link>
-      <Link href={linkedin_url} target="_blank" className={styles.social_media_icon}>
-        <LinkedinIcon />
-      </Link>
+      {contactSources.map(source => (
+        <Link
+          aria-label={source.ariaLabel}
+          className={styles.social_media_icon}
+          href={source.href}
+          key={source.href}
+          target="_blank"
+        >
+          {source.icon}
+        </Link>
+      ))}
     </AnimatedRender>
   );
 }
