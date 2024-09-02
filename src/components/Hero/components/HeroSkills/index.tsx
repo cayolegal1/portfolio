3ba42660f9c51ee8187908e8172a0444f2dadd61
Cayo Legal <cayolegal@hotmail.com>
2024-09-01 23:59:07 -0400

    add HeroSkills section

---
+++ src/components/Hero/components/HeroSkills/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { TypeAnimation } from "react-type-animation";
+import AnimatedRender from "@/core/components/AnimatedRender";
+import textStyles from "../HeroPresentation/HeroPresentation.module.css";
+import gradient from "../../../../core/components/TextGradient/TextGradient.module.css";
+
+export default function HeroSkills() {
+  return (
+    <AnimatedRender delay="1s">
+      <h1 className={textStyles.text} style={{width: "101%"}}>
+        Soy{" "}
+        <TypeAnimation
+          cursor={false}
+          sequence={["", 1500, "Desarrollador de "]}
+          speed={55}
+          style={{ whiteSpace: "normal" }}
+          wrapper="span"
+        />
+        <TypeAnimation
+          className={`${gradient.text_gradient_config} ${gradient.text_gradient}`}
+          cursor
+          sequence={["", 2300, "Software"]}
+          speed={55}
+          style={{ whiteSpace: "normal" }}
+          wrapper="span"
+        />
+      </h1>
+    </AnimatedRender>
+  );
+}
