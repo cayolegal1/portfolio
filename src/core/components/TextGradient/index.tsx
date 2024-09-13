ff24406dd2b37326a08f9cc81b909c88e700d960
Cayo Legal <cayolegal@hotmail.com>
2024-09-12 21:51:39 -0400

    add TextGradient ellipsis prop

--- src/core/components/TextGradient/index.tsx
+++ src/core/components/TextGradient/index.tsx
@@ -5,13 +5,19 @@ export default function TextGradient({
   as: Component = "span",
   children,
   className = "",
+  ellipsis = false,
   inHover = false,
   ...props
 }: TextGradientProps): JSX.Element {
   return (
     <Component
       {...props}
-      className={`${styles.text_gradient_config} ${inHover ? styles.in_hover : styles.text_gradient} ${className}`}
+      className={`
+        ${styles.text_gradient_config} 
+        ${ellipsis && styles.ellipsis} 
+        ${inHover ? styles.in_hover : styles.text_gradient} 
+        ${className}
+      `}
     >
       {children}
     </Component>
