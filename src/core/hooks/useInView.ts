ac952c60ffbbe5714113d65f6796c8580fc0ef13
Cayo Legal <cayolegal@hotmail.com>
2024-09-15 19:45:15 -0400

    add useInView hook

---
+++ src/core/hooks/useInView.ts
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+export const useInView = (
+  href: string,
+  config?: Partial<IntersectionObserverInit>,
+) => {
+  const [isInView, setIsInView] = useState(false);
+
+  const observe = (section: HTMLElement) => {
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      const entry = entries[0];
+      setIsInView(entry.isIntersecting);
+    };
+
+    const observer = new IntersectionObserver(callback, {
+      root: null,
+      rootMargin: "0px",
+      threshold: [0.5, 1.0],
+      ...config,
+    });
+
+    observer.observe(section);
+
+    return observer;
+  };
+
+  const onDocVisibilityChange = (
+    observer: IntersectionObserver,
+    section: HTMLElement,
+  ) => {
+    if (document.visibilityState === "hidden") {
+      observer.disconnect();
+    } else {
+      observe(section);
+    }
+  };
+
+  useEffect(() => {
+    if (!document) return;
+    const section = document.getElementById(href);
+    if (section) {
+      const observer = observe(section);
+      document.onvisibilitychange = () => {
+        onDocVisibilityChange(observer, section);
+      };
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, []);
+
+  return isInView;
+};
