c5153faad955385160a9199b8abc0c5abdb49eff
Cayo Legal <cayolegal@hotmail.com>
2024-09-22 16:29:14 -0400

    refactor code

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -3,30 +3,22 @@ import Section from "@/core/components/Section";
 import Text from "@/core/components/Text";
 import Timeline from "@/core/components/Timeline";
 import TimelineItem from "@/core/components/TimelineItem";
+import { useExperienceTranslation } from "@/core/hooks/useExperienceTranslation";
 import { SECTIONS } from "@/core/data/global";
 import styles from "./Experience.module.css";
 
 
 export default function Experience(): JSX.Element {
-  const t = useTranslations("Experience");
-  const translatedExperiences = Array.from(Array(Number(t("length"))).keys()).map((_, index) => {
-    return {
-      id: index,
-      company: t(`${index}.company`),
-      position: t(`${index}.position`),
-      from_date: t(`${index}.from_date`),
-      description: t(`${index}.description`),
-    };
-  });
-  ;
+  const translate = useTranslations();
+  const experiences = useExperienceTranslation();
   return (
     <Section id={SECTIONS.EXPERIENCE} withSeparator>
       <Text as="h3" size="title">
-        {t("title")}
+        {translate("title")}
       </Text>
       <div className={styles.timeline_container}>
         <Timeline>
-          {translatedExperiences.map(item => (
+          {experiences.map(item => (
             <TimelineItem key={item.id} experience={item} />
           ))}
         </Timeline>
