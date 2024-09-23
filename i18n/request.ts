49fdef7c1a51bd731e936c524e8296d8893749d5
Cayo Legal <cayolegal@hotmail.com>
2024-09-22 21:10:21 -0400

    code refactor

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,6 +1,5 @@
 import { cookies } from "next/headers";
 import { getRequestConfig } from "next-intl/server";
-import { redirect } from "next/navigation";
 
 export const LOCALES = {
   ENGLISH: "en",
@@ -10,15 +9,6 @@ export const LOCALES = {
 
 export const supportedLocales = [LOCALES.ENGLISH, LOCALES.SPANISH, LOCALES.PORTUGUES];
 export const defaultLocale = LOCALES.SPANISH;
-
-export const toggleLanguage = (formData: FormData) => {
-  const locale = formData.get("locale") as string;
-  cookies().set(
-    "locale",
-    supportedLocales.includes(locale) ? locale : defaultLocale,
-  );
-  redirect("/");
-};
 
 export default getRequestConfig(async () => {
   const locale = cookies().get("locale")?.value || defaultLocale;
--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,13 +1,16 @@
 "use server";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
-// import { defaultLocale, supportedLocales } from "../../i18n/request";
+import { supportedLocales } from "../../i18n/request";
 
 export const toggleLanguage = (formData: FormData) => {
   const locale = formData.get("locale") as string;
   const cookieStore = cookies();
-  if (cookieStore.get("locale")?.value !== locale) {
+  if (
+    cookieStore.get("locale")?.value !== locale &&
+    supportedLocales.includes(locale)
+  ) {
     cookieStore.set("locale", locale);
     redirect("/");
-  };
+  }
 };
