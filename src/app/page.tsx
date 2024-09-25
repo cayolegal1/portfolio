20870f4a3c5415f661d5e77fde10ffb11f2d78a2
Cayo Legal <cayolegal@hotmail.com>
2024-09-25 17:08:41 -0400

    remove contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Experience from "@/components/Experience";
-import Contact from "@/components/Contact";
 import styles from "./page.module.css";
 
 export default function App() {
@@ -11,7 +10,6 @@ export default function App() {
       <main className={styles.main}>
         <Hero />
         <Experience />
-        <Contact />
       </main>
     </>
   );
