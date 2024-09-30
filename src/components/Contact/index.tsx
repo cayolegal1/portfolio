0209f8b703f12fe1807748fefc494e1f1679fbb8
Cayo Legal <cayolegal@hotmail.com>
2024-09-29 21:37:09 -0400

    add contact in view animations

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,17 +1,18 @@
 import Section from "@/core/components/Section";
-import { SECTIONS } from "@/core/data/global";
-import Text from "@/core/components/Text";
+import AnimatedTitle from "@/core/components/AnimatedTitle";
+import AnimatedInView from "@/core/components/AnimatedInView";
 import ContactForm from "./components/ContactForm";
-import { useTranslations  } from "next-intl";
+import { useTranslations } from "next-intl";
+import { SECTIONS } from "@/core/data/global";
 
 export default function Contact() {
   const translate = useTranslations("Contact");
   return (
     <Section id={SECTIONS.CONTACT}>
-      <Text as="h3" size="title">
-        {translate("title")}
-      </Text>
-      <ContactForm />
+      <AnimatedTitle id="contact_title">{translate("title")}</AnimatedTitle>
+      <AnimatedInView animationType="fadeInDown" id="contact_form">
+        <ContactForm />
+      </AnimatedInView>
     </Section>
   );
 }
