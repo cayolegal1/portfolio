f7e94e3e32033c725d2a0b41d00897a809529ac6
Cayo Legal <cayolegal@hotmail.com>
2024-12-30 00:42:24 -0300

    add footer section

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,25 +1,28 @@
+// components
 import Section from "@/core/components/Section";
 import AnimatedInView from "@/core/components/Animated/AnimatedInView";
 import FooterItem from "./components/FooterItem";
-import Text from "@/core/components/Text";
+import Copyright from "./components/Copyright";
+
+// icons
 import LocationIcon from "@/core/components/Icons/LocationIcon";
 import EmailIcon from "@/core/components/Icons/EmailIcon";
 import GitHubIcon from "@/core/components/Icons/GithubIcon";
 import TwitterIcon from "@/core/components/Icons/TwitterIcon";
 import LinkedinIcon from "@/core/components/Icons/LinkedinIcon";
+
+// data
 import data from "@/core/data/user-info.json";
 import { SECTIONS } from "@/core/data/global";
+
 import styles from "./Footer.module.css";
 
 const personalData = [
-  { content: data.location, href: data.location_url, icon: <LocationIcon /> },
-  { content: data.email, href: `mailto:${data.email}`, icon: <EmailIcon /> },
-];
-
-const socialMediaData = [
   { content: data.github_user, href: data.github_url, icon: <GitHubIcon /> },
   { content: data.name, href: data.linkedin_url, icon: <LinkedinIcon /> },
   { content: data.twitter_user, href: data.twitter_url, icon: <TwitterIcon /> },
+  { content: data.location, href: data.location_url, icon: <LocationIcon /> },
+  { content: data.email, href: `mailto:${data.email}`, icon: <EmailIcon /> },
 ];
 
 export default function Footer() {
@@ -37,23 +40,12 @@ export default function Footer() {
         useId={false}
       >
         <div className={styles.footer_items_container}>
-          {socialMediaData.map(info => (
-            <FooterItem key={info.href} href={info.href} icon={info.icon}>
-              {info.content}
-            </FooterItem>
-          ))}
-        </div>
-
-        <div className={styles.footer_items_container}>
           {personalData.map(info => (
             <FooterItem key={info.href} href={info.href} icon={info.icon}>
               {info.content}
             </FooterItem>
           ))}
-
-          <Text size="caption">
-            &copy; 2024 Cayo Legal. Todos los derechos reservados.
-          </Text>
+          <Copyright />
         </div>
       </AnimatedInView>
     </Section>
