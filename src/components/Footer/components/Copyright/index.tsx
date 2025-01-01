d3b508c9798248229c36863289e72ad7e9473ef8
Cayo Legal <cayolegal@hotmail.com>
2025-01-01 20:33:25 -0300

    add dynamic copyright year

--- src/components/Footer/components/Copyright/index.tsx
+++ src/components/Footer/components/Copyright/index.tsx
@@ -5,6 +5,8 @@ import Text from "@/core/components/Text";
 export default function Copyright(): JSX.Element {
   const translate = useTranslations("Footer");
   return (
-    <Text size="caption">&copy; 2024 Cayo Legal. {translate("copyright")}</Text>
+    <Text size="caption">
+      &copy; {new Date().getFullYear()} Cayo Legal. {translate("copyright")}
+    </Text>
   );
 }
