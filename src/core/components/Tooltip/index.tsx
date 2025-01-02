f5f5a8eddf049fda35631a990759af482b971817
Cayo Legal <cayolegal@hotmail.com>
2025-01-01 21:43:11 -0300

    add tooltip component

---
+++ src/core/components/Tooltip/index.tsx
@@ -0,0 +1,17 @@
+import Text from "../Text";
+import type { TooltipProps } from "./Tooltip.types";
+import styles from "./Tooltip.module.css";
+
+export default function Tooltip({
+  children,
+  tooltip,
+}: TooltipProps): JSX.Element {
+  return (
+    <div className={styles.tooltip_container}>
+      {children}
+      <Text className={styles.tooltip_text} size="caption">
+        {tooltip}
+      </Text>
+    </div>
+  );
+}
