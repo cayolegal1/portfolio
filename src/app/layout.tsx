24d081a4701936dc3eae4d426d4d4e736c5aea08
Cayo Legal <cayo.legal@bepsa.com.py>
2025-01-08 09:26:08 -0300

    add og image fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default async function RootLayout({
       <head>
         <link rel="dns-prefetch" href="https://www.googletagmanager.com/" />
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
-        <meta property="og:title" content="Cayo Legal Developer" />
+        <meta property="og:title" content="Cayo Legal" />
         <meta property="og:description" content={websiteDescription} />
         <meta property="og:image" content={websiteImage} />
         <meta property="og:image:secure_url" content={websiteImage} />
