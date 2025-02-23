ac62eb2117aecaf0a7e42d287094db64ab395fe2
Cayo Legal <cayolegal@hotmail.com>
2025-02-23 00:11:54 -0300

    add will change css property in header

--- src/core/components/Icons/MeetIcon.tsx
+++ src/core/components/Icons/MeetIcon.tsx
@@ -7,17 +7,17 @@ export default function MeetIcon() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
+        strokeLinecap="round"
         strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <path
           stroke="currentColor"
           strokeLinejoin="round"
-          stroke-width="12"
+          strokeWidth="12"
           d="M138 140V52c0-8.837-7.163-16-16-16H58.16L22 72.16V140c0 8.837 7.163 16 16 16h84c8.837 0 16-7.163 16-16Zm32-88v88l-32-28V80l32-28Z"
         ></path>
       </g>
