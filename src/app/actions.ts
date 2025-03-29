8ec65ca6c51cb8816db3ee92bdc5935f3043165b
Cayo Legal <cayolegal@hotmail.com>
2025-03-29 08:11:21 -0300

    add fix of toggle language

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,16 +1,12 @@
 "use server";
 import { cookies } from "next/headers";
-import { supportedLocales, defaultLocale } from "../i18n/settings";
+import { supportedLocales } from "../i18n/settings";
 
 export const toggleLanguage = async (formData: FormData) => {
   const cookieStore = cookies();
   const cookieLocale = cookieStore.get("locale")?.value;
   const locale = formData.get("locale") as string;
-  if (
-    !(!cookieLocale && locale === defaultLocale) &&
-    cookieLocale !== locale &&
-    supportedLocales.includes(locale)
-  ) {
+  if (cookieLocale !== locale && supportedLocales.includes(locale)) {
     cookieStore.set("locale", locale);
 
     return {
