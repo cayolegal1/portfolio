3e302751e55e67884e6157ef9d620b574c9cd82f
Cayo Legal <cayolegal@hotmail.com>
2025-04-18 14:50:35 -0300

    fix hamburguer styles

--- src/components/Header/components/NavHamburguer/index.tsx
+++ src/components/Header/components/NavHamburguer/index.tsx
@@ -5,37 +5,10 @@ export default function NavHamburguer({
   expanded,
 }: NavHamburguerProps): JSX.Element {
   return (
-    <div
-      className={styles.hamburguer}
-      aria-controls="primary-navigation"
-      aria-expanded={expanded}
-    >
-      <svg fill="white" viewBox="0 0 100 100" width="27">
-        <rect
-          className={`${styles.line} ${styles.top}`}
-          width="80"
-          height="10"
-          x="10"
-          y="30"
-          rx="5"
-        ></rect>
-        <rect
-          className={`${styles.line} ${styles.middle}`}
-          width="80"
-          height="10"
-          x="10"
-          y="50"
-          rx="5"
-        ></rect>
-        <rect
-          className={`${styles.line} ${styles.bottom}`}
-          width="80"
-          height="10"
-          x="10"
-          y="70"
-          rx="5"
-        ></rect>
-      </svg>
+    <div className={`${styles.hamburguer}`} aria-expanded={expanded}>
+      <span className={`${styles.bar} ${styles.top}`}></span>
+      <span className={`${styles.bar} ${styles.middle}`}></span>
+      <span className={`${styles.bar} ${styles.bottom}`}></span>
     </div>
   );
 }
