0faab07b54715a19c9e5e5ffa545d164decbfabe
Cayo Legal <cayolegal@hotmail.com>
2025-04-25 18:41:00 -0300

    add aria values in hamburguer

--- src/components/Header/components/NavHamburguer/index.tsx
+++ src/components/Header/components/NavHamburguer/index.tsx
@@ -5,7 +5,12 @@ export default function NavHamburguer({
   expanded,
 }: NavHamburguerProps): JSX.Element {
   return (
-    <div className={`${styles.hamburguer}`} aria-expanded={expanded}>
+    <div
+      className={`${styles.hamburguer}`}
+      aria-expanded={expanded}
+      aria-label={expanded ? "Cerrar menú" : "Abrir menú"}
+      aria-roledescription="Botón para abrir o cerrar menú de navegación"
+    >
       <span className={`${styles.bar} ${styles.top}`}></span>
       <span className={`${styles.bar} ${styles.middle}`}></span>
       <span className={`${styles.bar} ${styles.bottom}`}></span>
