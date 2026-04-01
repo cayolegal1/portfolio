d0bf2d2097854dbf70577ace288b3f8c961b04a3
Cayo Legal <cayolegal@hotmail.com>
2026-04-01 14:54:13 -0300

    feat(seo): add seo improvements in json ld script

--- src/components/JsonLdScript.tsx
+++ src/components/JsonLdScript.tsx
@@ -13,7 +13,9 @@ export default function JsonLDScript(): JSX.Element {
           "@context": "https://schema.org",
           "@type": "Person",
           name: data.name,
-          jobTitle: "Software Developer",
+          description:
+            "Desarrollador FullStack especializado en web y mobile usando tecnologías modernas.",
+          jobTitle: "FullStack Developer",
           url: data.website_url,
           image: `${data.website_url}/social-img.webp`,
           sameAs: [data.linkedin_url, data.github_url],
@@ -21,6 +23,34 @@ export default function JsonLDScript(): JSX.Element {
             "@type": "PostalAddress",
             addressCountry: "Paraguay",
           },
+          worksFor: {
+            "@type": "Organization",
+            name: "Bepsa",
+            url: "https://www.bepsa.com.py",
+          },
+          hasOccupation: {
+            "@type": "Occupation",
+            name: "FullStack Developer",
+            occupationLocation: {
+              "@type": "Place",
+              name: "Paraguay",
+            },
+          },
+          knowsAbout: [
+            "HTML",
+            "CSS3",
+            "Tailwind CSS",
+            "JavaScript",
+            "TypeScript",
+            "React",
+            "React Native",
+            "NextJS",
+            "NodeJS",
+            "Express",
+            "NestJS",
+          ],
+          skill: ["Agile", "CI/CD", "Docker", "Testing"],
+          knowsLanguage: ["Spanish", "English"],
         }),
       }}
     />
