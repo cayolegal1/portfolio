7f4d8b493c4a45f4013809aedac83c2ab030e8bf
Cayo Legal <cayolegal@hotmail.com>
2026-04-26 16:16:43 -0300

    chore(refactor): code refactor

--- src/components/Projects/components/ProjectImage/index.tsx
+++ src/components/Projects/components/ProjectImage/index.tsx
@@ -7,44 +7,20 @@ export default function ProjectImage({ ...props }: ProjectImageProps) {
   const [isFullScreen, setIsFullScreen] = useState(false);
   const imgRef = useRef<HTMLImageElement>(null);
 
-  const toggleFullScreen = () => {
-    if (!imgRef.current) {
-      return;
-    }
-
-    if (imgRef.current.classList.contains(styles["full-screen"])) {
-      setIsFullScreen(false);
-      imgRef.current.classList.remove(styles["full-screen"]);
-    } else {
-      imgRef.current.classList.add(styles["full-screen"]);
-      setIsFullScreen(true);
-    }
-  };
-
   useEffect(() => {
     const handleOutsideClick = (e: MouseEvent) => {
       if (!imgRef.current) return;
 
-      const isWithFullScreenClass = imgRef.current.classList.contains(
-        styles["full-screen"],
-      );
-
-      if (isFullScreen) {
+      if (!imgRef.current.contains(e.target as Node)) {
         setIsFullScreen(false);
       }
-
-      if (!isWithFullScreenClass) return;
-
-      if (imgRef.current.contains(e.target as Node)) return;
-      imgRef.current.classList.remove(styles["full-screen"]);
     };
 
     document.addEventListener("click", handleOutsideClick);
-
     return () => {
       document.removeEventListener("click", handleOutsideClick);
     };
-  }, [isFullScreen]);
+  }, []);
 
   return (
     <>
@@ -52,8 +28,10 @@ export default function ProjectImage({ ...props }: ProjectImageProps) {
         {...props}
         ref={imgRef}
         loading="lazy"
-        onClick={toggleFullScreen}
-        className={styles.image}
+        onClick={() => setIsFullScreen(prev => !prev)}
+        className={`${styles.image} ${
+          isFullScreen ? styles["full-screen"] : ""
+        }`}
       />
 
       <div className={isFullScreen ? styles.overlay : ""} />
